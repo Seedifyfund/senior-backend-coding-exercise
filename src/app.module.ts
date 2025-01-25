60a20f868196a09022d5eb8ae3fc41532e1b65cc
Ryuytyuu-Greseven <rukku93223@gmail.com>
2025-01-25 09:29:16 +0530

    removed shared module import.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { OrderModule } from './order/order.module';
 import { AppController } from './app.controller';
 import { ProductModule } from './product/product.module';
 import { UsersModule } from './users/users.module';
-import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [OrderModule, ProductModule, UsersModule, SharedModule],
+  imports: [OrderModule, ProductModule, UsersModule],
   controllers: [AppController],
   providers: [],
 })
